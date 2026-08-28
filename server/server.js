b294f4cb20290d659ac77a5851095ecd83c13030
MAICOM MATEUS RAMPI <133053804+MaicomMRampi@users.noreply.github.com>
2026-08-27 23:39:34 -0300

    chore: iniciando o servidor com o express e instalando as dependencias

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import 'dotenv/config';
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
