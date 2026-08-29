d0537f067d54301aa9b63c43cef836cd8f8e2b76
MAICOM MATEUS RAMPI <133053804+MaicomMRampi@users.noreply.github.com>
2026-08-29 00:38:35 -0300

    fix: ajustando o erro do middware

--- client/proxy.ts
+++ client/proxy.ts
@@ -1,31 +1,31 @@
 import { NextResponse } from "next/server"
 import { NextRequest } from "next/server"
 
-export function proxy(req: NextRequest) {
-    const token = req.cookies.get('access_app')
-    const { pathname } = req.nextUrl
+export function proxy(request: NextRequest) {
+  const token = request.cookies.get('help_desk_token')
+  const { pathname } = request.nextUrl
 
-    // SE NÃO TEM TOKEN E TENTAR ACESSAR /LOGIN PERMITE
-    
-    if(!token && pathname === '/login'){
-        return NextResponse.next()
-    }
+  // SE NÃO TEM TOKEN E TENTAR ACESSAR /LOGIN PERMITE
 
-    /* ======= TEM TOKEN E TENTAR ACESSAR /LOGIN REDIRECIONA PARA HOME =====*/
-
-    if (token && pathname === '/login') {
-        return NextResponse.redirect(new URL("/home", req.url))
-    }
-
-    /* ====== SE NÃO TEM TOKEN REDIRECIONA PARA O LOGIN ======*/
-
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", req.url))
-    }
-
+  if (!token && pathname === '/login') {
     return NextResponse.next()
+  }
+
+  /* ======= TEM TOKEN E TENTAR ACESSAR /LOGIN REDIRECIONA PARA HOME =====*/
+
+  if (token && pathname === '/login') {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+
+  /* ====== SE NÃO TEM TOKEN REDIRECIONA PARA O LOGIN ======*/
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url))
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
-    matcher: ["/((?!_next/static|_next/image|favicon.ico|public|login|.*\\.(?:png|jpg|jpeg|svg)$).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|public|.*\\.(?:png|jpg|jpeg|svg)$).*)"],
 }
