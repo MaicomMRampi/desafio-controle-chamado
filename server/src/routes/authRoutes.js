d224ed3b9a019b44b6e9278fc2619a25e394635c
MAICOM MATEUS RAMPI <133053804+MaicomMRampi@users.noreply.github.com>
2026-08-29 14:56:43 -0300

    style: ajustando a nomenclatura do import e o status de retorno

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login } from '../controllers/authControllers.js'
+import { login } from '../controllers/authController.js'
 // import 
 const router = express.Router()
 
--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -15,14 +15,14 @@ export async function AuthLogin({ password, email }) {
   try {
     const user = await db.query(sqlUser, [email])
 
-    if (user.rowCount === 0) return returnCode(true, 401, 'Credenciais inválidas')
+    if (user.rowCount === 0) return returnCode(true, 401, 'E-mail ou senha inválidos')
     if (user?.rows[0].status === false) return returnCode(true, 401, 'Usuário inativo, contate o administrador do sistema')
 
     const userPassword = user.rows[0].senha
 
     const compare = await comparePassword(password, userPassword)
 
-    if (!compare) return returnCode(true, 401, 'Senha inválida')
+    if (!compare) return returnCode(true, 401, 'E-mail ou senha inválidos')
 
     return {
       error: false,
