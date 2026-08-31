9a3c3504515054a4033d5a4bfcc7972a08752231
MAICOM MATEUS RAMPI <133053804+MaicomMRampi@users.noreply.github.com>
2026-08-31 19:04:09 -0300

    style: ajustando o layoult do login, somente alteração visual

--- client/app/(pages)/login/page.tsx
+++ client/app/(pages)/login/page.tsx
@@ -1,18 +1,18 @@
-"use client";
+"use client"
 
-import { showErrorToast, showSuccessToast } from "@/app/components/toastDefault";
-import { api } from "@/app/lib/axiosInstance";
-import { Avatar, Button, Input } from "@heroui/react";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
+import { showErrorToast, showSuccessToast } from "@/app/components/toastDefault"
+import { api } from "@/app/lib/axiosInstance"
+import { Avatar, Button, Input, Label } from "@heroui/react"
+import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
 
 interface FormData {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
 export default function Login() {
-  const { register, handleSubmit, formState: { errors }, } = useForm({ defaultValues: { email: "", password: "", }, });
+  const { register, handleSubmit, formState: { errors }, } = useForm({ defaultValues: { email: "", password: "", }, })
   const router = useRouter()
 
   // ============= FUNÇÕES ============ 
@@ -26,32 +26,44 @@ export default function Login() {
       }
     } catch (error: undefined | any) {
       showErrorToast(error?.response?.data?.message)
-      console.log(`Erro ao fazer login', ${error?.response?.data?.message}`);
+      console.log(`Erro ao fazer login', ${error?.response?.data?.message}`)
     }
   }
 
   return (
-    <div className="w-full h-full grid grid-cols-1 md:grid-cols-12 bg-[#016a94f5]">
-      <div className="col-span-12 md:col-span-8 bg-center sx:hidden" style={{ clipPath: "polygon(0 0, 100% 0, 80% 100%, 0% 100%)" }}>
-        <img src={`../images/5.jpg`} alt="1" className={`w-full h-screen transition-opacity duration-1000 ${false ? "opacity-100" : "opacity-0"}`} />
+    <div className="grid grid-cols-12 min-h-screen w-full">
+      <div className="hidden md:block md:col-span-8 w-full h-full overflow-hidden">
+        <img
+          src="/images/callendar.png"
+          alt="Ilustração do Calendário"
+          className="w-full h-full object-cover"
+        />
       </div>
-      <form onSubmit={handleSubmit((data) => onSubmitForm(data))} className="w-[60%] h-full flex items-center justify-center col-span-12 md:col-span-4 flex-col gap-3">
-        <Avatar size="lg">
-          <Avatar.Image alt="Logo bold energy" src="../images/logo.png" />
-          <Avatar.Fallback>JD</Avatar.Fallback>
-        </Avatar>
+      <div className="col-span-12 md:col-span-4 flex items-center justify-center p-6">
+        <form
+          onSubmit={handleSubmit((data) => onSubmitForm(data))}
+          className="flex flex-col gap-5 w-full max-w-sm bg-white p-8 rounded-2xl shadow-xl border border-slate-100"
+        >
+          <h2 className="text-2xl font-bold text-slate-800">Bem-vindo de volta 👋</h2>
+          <p className="text-sm text-slate-500 -mt-3">Acesse sua conta para continuar.</p>
+          <div className="w-full">
+            <Label>E-mail</Label>
+            <Input className="h-10" fullWidth {...register('email', { required: 'Email é obrigatório', pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Email inválido" } })} placeholder="Digite seu email" />
+          </div>
 
-        <Input className="h-10" fullWidth {...register('email', { required: 'Email é obrigatório', pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Email inválido" } })} placeholder="Digite seu email" />
+          {/* ========= ERROS RELACIONADOS AO EMAIL ==========*/}
+          {errors.email && <span className="text-red-500">{errors.email.message}</span>}
 
-        {/* ========= ERROS RELACIONADOS AO EMAIL ==========*/}
-        {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+          <div>
+            <Label>Senha</Label>
+            <Input type="password" className="h-10" fullWidth {...register('password', { required: 'Senha é obrigatória' })} placeholder="Digite sua senha" />
+          </div>
+          {/* ========== ERROS RELACIONADO A SENHA ================= */}
+          {errors.password && <span className="text-red-500">{errors.password.message}</span>}
 
-        <Input type="password" className="h-10" fullWidth {...register('password', { required: 'Senha é obrigatória' })} placeholder="Digite sua senha" />
-        {/* ========== ERROS RELACIONADO A SENHA ================= */}
-        {errors.password && <span className="text-red-500">{errors.password.message}</span>}
-
-        <Button fullWidth type="submit">Entrar</Button>
-      </form>
+          <Button fullWidth type="submit">Entrar</Button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
